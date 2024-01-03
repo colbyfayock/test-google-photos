3bc7a9380abc2b8755cc334f41db37ec4c1a9ee9
Colby Fayock <fayock@gmail.com>
2024-01-03 15:52:37 -0500

    fixing theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,19 @@ const config: Config = {
   daisyui: {
     themes: [
       {
+        // Extends Light: https://github.com/saadeghi/daisyui/blob/master/src/theming/themes.js#L190
         mytheme: {
+          "color-scheme": "light",
           "primary": "#0d9aff",
-          "neutral": colors.slate['500']
+          "secondary": "oklch(69.71% 0.329 342.55)",
+          "secondary-content": "oklch(98.71% 0.0106 342.55)",
+          "accent": "oklch(76.76% 0.184 183.61)",
+          "neutral": colors.slate['500'],
+          "neutral-content": "#D7DDE4",
+          "base-100": "oklch(100% 0 0)",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
         },
       },
       "dark"
